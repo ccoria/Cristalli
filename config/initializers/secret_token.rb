3e7a3803980b1df20e570f259cff10bc07cfa3d9
Cassiano Coria Neto <cassiano.coria@yahoo.com.br>
2011-09-12 00:16:23 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cristalli::Application.config.secret_token = '35609682d3ede0b62aad98308e4d01628bfd763191e49ff594419364ef550819565732e511914d32de71fb891cd1599f267e6b8dfbe16e4475d58a194769826e'
