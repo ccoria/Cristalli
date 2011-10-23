8f484a8b61f6d0d7593a3e971633bac22d8cbb25
Cassiano Coria Neto <cassiano.coria@yahoo.com.br>
2011-10-23 19:39:57 -0200

    more structured pages and contact first form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,17 +1,22 @@
 class PagesController < ApplicationController
   def home
+    @title = "Home"
   end
 
   def concept
+    @title = "Conceito"
   end
 
   def modules
+    @title = "Modulos"
   end
 
   def photos
+    @title = "Fotos"
   end
 
   def contact
+    @title = "Contato"
   end
 
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -7,6 +7,13 @@ describe PagesController do
       get 'home'
       response.should be_success
     end
+    
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title",
+                        :content => "Cristalli")
+    end
+    
   end
 
   describe "GET 'concept'" do
