998070f565f6cc0162e0582215e329ae2e9d8600
Cassiano Coria Neto <cassiano.coria@yahoo.com.br>
2011-11-06 21:51:26 -0200

    rspec passing for all pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
     it "should be successful" do
@@ -10,16 +11,19 @@ describe PagesController do
     
     it "should have the right title" do
       get 'home'
-      response.should have_selector("title",
-                        :content => "Cristalli")
+      response.should have_selector("title", :content => "Home")
     end
-    
   end
 
   describe "GET 'concept'" do
     it "should be successful" do
       get 'concept'
       response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'concept'
+      response.should have_selector("title", :content => "Conceito")
     end
   end
 
@@ -28,6 +32,11 @@ describe PagesController do
       get 'modules'
       response.should be_success
     end
+    
+    it "should have the right title" do
+      get 'modules'
+      response.should have_selector("title", :content => "Modulos")
+    end
   end
 
   describe "GET 'photos'" do
@@ -35,12 +44,22 @@ describe PagesController do
       get 'photos'
       response.should be_success
     end
+    
+    it "should have the right title" do
+      get 'photos'
+      response.should have_selector("title", :content => "Fotos")
+    end
   end
 
   describe "GET 'contact'" do
     it "should be successful" do
       get 'contact'
       response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title", :content => "Contato")
     end
   end
 
