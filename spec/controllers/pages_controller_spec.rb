237674ec5543242ebdd2b05d3176bb8da80d939f
Cassiano Coria Neto <cassiano.coria@yahoo.com.br>
2012-02-24 00:55:20 -0200

    temporary fix for images pagination

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -51,6 +51,24 @@ describe PagesController do
       get 'photos'
       response.should have_selector("title", :content => "Fotos")
     end
+    
+    it "should have the 'next' but not the 'prev' pagination link working in the first page" do
+      get 'photos'
+      assert_select "a[id=next_photo]", true
+      assert_select "a[id=previous_photo]", false
+    end
+    
+    it "should have the 'prev' but not the 'next' pagination link working in the last page" do
+      get :photos, :id => 42
+      assert_select "a[id=next_photo]", false
+      assert_select "a[id=previous_photo]", true
+    end
+    
+    it "should have the 'next' and 'previous' pagination link working all other pages" do
+      get :photos, :id => 1
+      assert_select "a[id=previous_photo][href='/fotos/0']"
+      assert_select "a[id=next_photo][href='/fotos/2']"
+    end
   end
 
   describe "GET 'contact'" do
